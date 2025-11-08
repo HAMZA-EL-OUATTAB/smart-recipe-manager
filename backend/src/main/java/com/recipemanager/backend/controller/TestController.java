292c1dc984b7d89ed67d60d0c0dd1b9d476bf01c
HAMZA-EL-OUATTAB <hamza.elouattab23@gmail.com>
2025-11-08 23:38:31 +0000

    Configure Spring Boot backend with test endpoints

---
+++ backend/src/main/java/com/recipemanager/backend/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.recipemanager.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/test")
+    public Map<String,Object> test(){
+        Map<String,Object> response = new HashMap<>();
+        response.put("status","success");
+        response.put("message","Backend is running");
+        response.put("timestamp",LocalDateTime.now());
+        response.put("developer","Hamza EL Ouattab");
+        return response;
+    }
+
+    @GetMapping("/health")
+    public Map<String,Object> health(){
+        Map<String,Object> response = new HashMap<>();
+        response.put("status","UP");
+        response.put("sevice","Recipe Manager API");
+        return response;
+    }
+
+
+
+}
